dbe4bde473d9c517f37e8f75063bbb894edb1aa3
CarlRTS <cdmr171@gmail.com>
2025-02-12 09:47:10 -0400

    Botones y user bien

--- main.js
+++ main.js
@@ -14,10 +14,12 @@ const user = document.createElement("div");
 user.style.position = "absolute";
 user.style.top = "60px";
 user.style.left = "50px";
-user.style.transform = "translateX(50)";
+user.style.transform = "translateX(1000)";
 user.style.fontFamily = "Roboto";
 user.style.fontSize = "20px";
 user.style.color = "#fff";
+user.style.background = "#2596be"
+user.style.top = 10;
 user.style.alignSelf = "center";
 user.style.backgroundColor = "0000FF";
 
@@ -30,6 +32,22 @@ document.body.appendChild(user);
 function mostrar_user(){
     localStorage.getItem("Username")
 }
+
+const boton = document.querySelector("button")
+boton.addEventListener('click', () =>{
+    let start = document.querySelector ("button")
+    start.innerHTML = 'El juego empezó'
+})
+
+
+    
+
+
+const recuadro = document.querySelector("red")
+recuadro.addEventListener('click', () =>{
+    let seleccionado = document.querySelector("red")
+    seleccionado.click()
+})
 
 
 
