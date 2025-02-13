4bae9f82a29d4c5580f302ebb571e5efaaf8e62e
sergioob4 <144851074+sergioob4@users.noreply.github.com>
2025-02-13 07:30:35 -0400

    Lógica parte I

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const nivel = 10;
+const red = document.getElementById('red');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+const yellow = document.getElementById('yellow');
+
+
 
 (function clear(){
     localStorage.clear();
@@ -44,9 +51,69 @@ boton.addEventListener('click', () =>{
 
 
 
+const buttonred = document.querySelector(".red");
+buttonred.addEventListener("click",() => {
+    buttonred.classList.toggle("active");
+});
+
+
+const buttonblue = document.querySelector(".blue");
+buttonblue.addEventListener("click",() => {
+    buttonblue.classList.toggle("active");
+});
+
+
+const buttongreen = document.querySelector(".green");
+buttongreen.addEventListener("click",() =>{
+    buttongreen.classList.toggle("active");
+});
+
+const buttonyellow = document.querySelector(".yellow");
+buttonyellow.addEventListener("click", () =>{
+    buttonyellow.classList.toggle("active");
+});
 
 
 
+function generarSecuencia(){
+    const secuencia = new Array(nivel).fill(0).map(n => Math.floor(Math.random() * 4) )
+
+    console.log(secuencia);
+    return secuencia;
+}
+
+function transformNumAColor(numero){
+    switch(numero){
+        case 0:
+            return 'red'
+        case 1:
+            return 'blue'
+        case 2:
+            return 'green'
+        case 3:
+            return 'yellow'
+    }
+}
+
+function transformColorANum(color){
+    switch(color){
+        case 'red':
+            return 0
+        case 'blue':
+            return 1 
+        case 'green':
+            return 2
+        case 'yellow':
+            return 3
+    }
+}
+
+
+(function prueba(){
+    const v =generarSecuencia();
+    const colores = v.map(transformColorANum)
+    console.log(transformColorANum(v));
+})();
 
 
 
