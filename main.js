2527d6936964256f9ad5ed6a00c24140267afda8
CarlRTS <cdmr171@gmail.com>
2025-02-13 14:03:56 -0400

    Arreglo

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-const nivel = 10;
-const red = document.getElementById('red');
-const blue = document.getElementById('blue');
-const green = document.getElementById('green');
-const yellow = document.getElementById('yellow');
-var audio = document.querySelector("audio");
 
+var audio = document.querySelector("audio");
+const nivel = 10;
 
 
 (function clear(){
@@ -43,33 +39,6 @@ function mostrar_user(){
 }
 
 
-const buttonred = document.querySelector(".red");
-const buttonblue = document.querySelector(".blue");
-const buttongreen = document.querySelector(".green");
-const buttonyellow = document.querySelector(".yellow");
-const boton = document.querySelector("button")
-boton.addEventListener('click', () =>{
-    let start = document.querySelector ("button")
-    audio.play();
-    start.innerHTML = 'El juego empezó'
-    generarSecuencia();
-    
-    /*buttonred.addEventListener("click",() => {
-        buttonred.classList.toggle("active");
-    });
-    buttonblue.addEventListener("click",() => {
-        buttonblue.classList.toggle("active");
-    });
-    buttongreen.addEventListener("click",() =>{
-        buttongreen.classList.toggle("active");
-    });
-        buttonyellow.addEventListener("click", () =>{
-        buttonyellow.classList.toggle("active");
-});*/
-    
-})
-
-
 function generarSecuencia(){
     const secuencia = new Array(nivel).fill(0).map(n => Math.floor(Math.random() * 4) )
     if(secuencia[0]==0){
@@ -88,39 +57,37 @@ function generarSecuencia(){
     console.log(secuencia);
     return secuencia;
 }
+const buttonred = document.querySelector(".red");
+const buttonblue = document.querySelector(".blue");
+const buttongreen = document.querySelector(".green");
+const buttonyellow = document.querySelector(".yellow");
+const boton = document.querySelector("button")
+boton.addEventListener('click', () =>{
+    let start = document.querySelector ("button")
+    audio.play();
+    start.innerHTML = 'El juego empezó'
+    generarSecuencia();
+    
+    
+    /*buttonred.addEventListener("click",() => {
+        buttonred.classList.toggle("active");
+    });
+    buttonblue.addEventListener("click",() => {
+        buttonblue.classList.toggle("active");
+    });
+    buttongreen.addEventListener("click",() =>{
+        buttongreen.classList.toggle("active");
+    });
+        buttonyellow.addEventListener("click", () =>{
+        buttonyellow.classList.toggle("active");
+});*/
+    
+})
 
-function transformNumAColor(numero){
-    switch(numero){
-        case 0:
-            return 'red'
-        case 1:
-            return 'blue'
-        case 2:
-            return 'green'
-        case 3:
-            return 'yellow'
-    }
-}
-
-function transformColorANum(color){
-    switch(color){
-        case 'red':
-            return 0
-        case 'blue':
-            return 1 
-        case 'green':
-            return 2
-        case 'yellow':
-            return 3
-    }
-}
 
 
-(function prueba(){
-    const v =generarSecuencia();
-    const colores = v.map(transformColorANum)
-    console.log(transformColorANum(v));
-})();
+
+
 
 
 
